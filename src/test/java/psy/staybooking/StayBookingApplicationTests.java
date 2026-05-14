835e4c0d46d7a37ea3f18cf7e79105a28b444835
develup_psy <develup_psy@github.com>
2026-05-14 12:40:34 +0900

    chore: 초기 프로젝트 설정

---
+++ src/test/java/psy/staybooking/StayBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package psy.staybooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
