f85387ff6be1e2e383afe3b578538dc425bd8de0
develup_psy <develup_psy@github.com>
2026-05-14 22:13:17 +0900

    feat: 운영 모드 전환 흐름을 추가한다

--- src/main/java/psy/staybooking/system/repository/SystemModeRepository.java
+++ src/main/java/psy/staybooking/system/repository/SystemModeRepository.java
@@ -1,7 +1,16 @@
 package psy.staybooking.system.repository;
 
+import java.util.Optional;
+import jakarta.persistence.LockModeType;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import psy.staybooking.system.domain.SystemMode;
 
 public interface SystemModeRepository extends JpaRepository<SystemMode, Byte> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select systemMode from SystemMode systemMode where systemMode.systemModeId = :systemModeId")
+    Optional<SystemMode> findByIdForUpdate(@Param("systemModeId") Byte systemModeId);
 }
